b6d502952011cb5b6771ad6a64248de8f9434adb
Andrew Maciel Queiroz <amaciel@voxdata.local>
2022-01-26 18:15:44 -0300

    estudo
    teste v1

---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="newUser.name" label="Nome" />
+      <q-input v-model="newUser.email" label="Email" />
+      <q-input v-model="newUser.password" label="Senha" />
+      <q-btn color="primary" icon="check" label="Cadastrar" @click="submit()" />
+      {{ newUser }}
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      newUser: {
+        name: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      axios
+        .post("https://web.voxdatati.com.br:4443/api/users", this.newUser)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
---
+++ src/pages/users/usuario.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="newUser.nome" label="Nome" />
+      <q-input v-model="newUser.email" label="Email" />
+      <q-input v-model="newUser.senha" label="Senha" />
+      {{ newUser }}
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      newUser: {
+        nome: "",
+        email: "",
+        senha: ""
+      }
+    };
+  }
+};
+axios
+  .get("https://web.voxdatati.com.br:4443/api/authenticate")
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+</script>
